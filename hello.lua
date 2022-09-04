556e74b066facdbedb82c307a45945d2f6b178a2
joeym297 <112829117+joeym297@users.noreply.github.com>
2022-09-04 19:56:28 -0400

    Update hello.lua

--- hello.lua
+++ hello.lua
@@ -5,86 +5,9 @@ shared.stop = true -- stops the player at any time if true
 shared.ftime = 9*60 + 25 -- time in seconds for the song to finish (extended by |)
 shared.delay = nil -- delay overides the ftime
 shared.tempo = nil -- tempo overides the delay
-f D f D f a d s [6p]
-0 e t u p [3a]
-0 W u O a [6s]
-0 e u f D f
-D f a d s [6p]
-0 e t u p [3a]
-0 W y s a [6p] 0 e f D f D f a d s [6p] 0 e t u p [3a] 0 W u O a [6s] 0 e u f D f
-D f a d s [6p]
-0 e t u p [3a]
-0 W y s a [6p]
-0 e a s d [8f]
-w t o g f [5d]
-w r i f d [6s]
-0 e u d s [3a]
-0 u u f u f
-f x D f D f
-D f D f D f
-D f a d s [6p]
-0 e t u p [3a]
-0 W u O a [6s]
-0 e u f D f
-D f a d s [6p]
-0 e t u p [3a]
-0 W y s a [6p]
-0 e a s d [8f]
-w t o g f [5d]
-w r i f d [6s]
-0 e u d s [3a]
-0 u u f u f
-f x D f D f
-D f D f D f
-D f a d s [6p]
-0 e t u p [3a]
-0 W u O a [6s]
-0 e u f D f
-D f a d s [6p]
-0 e t u p [3a]
-0 W y s a [6p]
-0 e [etus] [etis] [wts] [qs]
-e t e [tg] e f [qf]
-E [yd] E [yj] E j [qj]
-[uh] [eqg] [uf] [eqd] [us] [qp]
-e [tp] e P [tp] o [ep] P [qs]
-e t e [td] [ed] [0f]
-e t [ef] [9yg] [qp] [ws]
-u w [ud] s a s [wd] i a [tus]
-h o h p h [ioa] h [uos] h [yiod] h [tuof]
-h l k [qej] h g f [wrd] h g d [tus]
-h o h p h [ioa] h [uos] h [yiod] h [tuof]
-h l k [qej] h g f [wrd] h g d [wrf]
-g f D f a f D f a f D f
-a f D f a f D f D f D f D f D f a d s [6p] 0 e t u p [3a] 0 W u O a [6s] 0 e u f D f D f a d s [6p] 0 e t u p [3a] 0 W y s a [6p] 0 e a s d [8f] w t o g f [5d] w r i f d [6s] 0 e u d s [3a] 0 u u f u f f x D f D f D f D f D f D f a d s [6p] 0 e t u p [3a] 0 W u O a [6s] 0 e u f D f D f a d s [6p] 0 e t u p [3a] 0 W y s a [6p] 6 6 6 6 6 [6uos] 6 6 6 6 6 [6ipd] 6 6 6 [s6f] [6dg] [o6dg] 6 6 6 [o6dg] 6 [6psf] 6 6 6 6 6 [26id] [26] [26] [26] [26us] [26ya] [26tp] [26] [26] [26] [26tp] [26] [36tp] [36] [36us] [36] [53ya] [35] [6tp] 6 6 6 6 6 [6uos] 6 6 6 6 6 [6ipd] 6 6 6 [s6f] [6dg] [6dg] 6 6 6 [6dg] 6 [6dg] ^ ^ ^ ^ ^ [o6d] ^ ^ ^ [6id] [6ys] [yi6p] ^ ^ ^ [6yip]
-^ [7yio] 7 7 7 [7yio]
-7 [8tup]
-[0wua]
-[6e] t u p s f [etud]
-s a [etup] s f j l x [etuz]
-l k [etuj] l x b m x [etuz]
-m n [etuB] bVvCcxZ zLlkJjHhGg
-s [6p] 0 e t u
-p [3a] 0 W u O
-a [6s] 0 e u f
-D f D f a d
-s [6p] 0 e t u
-p [3a] 0 W y s
-a [6p] 0 e a s
-d [8f] w t o g
-f [5d] w r i f
-d [6s] 0 e u d
-s [3a] 0 u u f
-u f f x D f
-D f D f D f
-D f D f a d
-s [6p] 0 e t u
-p [3a] 0 W u O
-a [6s] 0 e u f
-D f D f a d
-s [6p] 0 e t u
-p [3a] 0 W u s
-a [6p]
-s[6p]0etu
-p[3a]0Wus
-a[6p]
+fDfDfads [p6] 0e tup [a3] 0W uOa [s6] 0e u fDfDfads [p6] 0e tup [a3] 0W usa [p6] 0e
+fDfDfads [p6] 0e tup [a3] 0W uOa [s6] 0e u fDfDfads [p6] 0e tup [a3] 0W usa [p6] 0e
+asd [f8] wt ogf [d5] wr ifd [s6] 0e uds [a3] 0 uufuff x DfDfDfDfDfads [p6] 0e tup [a3] 0W uOa [s6] 0e u fDfDfads
+[p6] 0e tup [a3] 0W usa [p6] 0e fDfDfads [p6] 0e tup [a3] 0W uOa [s6] 0e u fDfDfads [p6] 0e tup [a3] 0W usa [p6] 0e
+fDfDfads [p6] 0e tup [a3] 0W usa [p6] 0e asd [f8] wt ogf [d5] wr ifd [s6] 0e uds [a3] 0 uufuffx DfDfDfDfads [p6] 0e tup
+[a3] 0W uOa [s6] 0e u fDfDfads [p6] 0e tup [a3] 0W usa [6t0p]
